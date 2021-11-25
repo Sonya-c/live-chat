21d845d618d1daae132167f618e0fd13fa847c11
DavidCB <70921381+DavidC031@users.noreply.github.com>
2021-11-25 05:18:58 -0500

    Update Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -46,7 +46,34 @@ export default function Auth() {
                     disabled={!validateForm()}>
                     Ingresar
                 </button>
-
+// lista de sala desplegable
+       <form action="chat.html">
+              <div class="form-control">
+                <label for="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Enter username..."
+                  required
+                />
+              </div>
+              <div class="form-control">
+                <label for="room">Room</label>
+                <select name="room" id="room">
+                  <option value="JavaScript">JavaScript</option>
+                  <option value="Python">Python</option>
+                  <option value="PHP">PHP</option>
+                  <option value="C#">C#</option>
+                  <option value="Ruby">Ruby</option>
+                  <option value="Java">Java</option>
+                </select>
+              </div>
+              <button type="submit" class="btn">Join Chat</button>
+            </form>
+                
+                
+            </form>
             </form>
         </main>
     );
